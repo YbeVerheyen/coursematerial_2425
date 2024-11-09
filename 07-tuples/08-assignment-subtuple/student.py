91b3e5aeec8eb45125821b0d077bf41c88654f7d
YbeVerheyen <verheyenybe@gmail.com>
2024-11-09 17:56:10 +0100

    9/11 passing-percentage

--- 07-tuples/08-assignment-subtuple/student.py
+++ 07-tuples/08-assignment-subtuple/student.py
@@ -1,1 +1,16 @@
-# write your code here
+# write your code here 
+#def subtuple(xs, ys):
+#    return str(ys) in str(xs)
+   # 2,3,4 in (1,2,3,4,5)
+
+#print(subtuple((1,2,3,4,5),(2,3,4)))
+
+def subtuple(xs, ys):
+    if not ys:
+        return True
+    for i in range(len(xs) - len(ys) + 1): 
+        if xs[i:i + len(ys)] == ys:
+            return True 
+    return False
+
+#print(subtuple((1,2,3,4,5), (2,3,4)))  # Output: True
--- 07-tuples/10-assignment-passing-percentage/student.py
+++ 07-tuples/10-assignment-passing-percentage/student.py
@@ -1,1 +1,9 @@
 # write your code here
+def passing_percentage(grades):
+    passed = sum(i >= 10 for i in grades)
+    if not grades:
+        return 0
+    
+    percentage = (passed / len(grades)) * 100
+    
+    return int(percentage)
